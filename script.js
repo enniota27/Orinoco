d2b57a894dc51a7bb80954200808e9a5ea6f86b8
Antoine NAGY <nagy.antoine@gmail.com>
2020-05-23 20:23:14 +0200

    Ajout commentaire

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        //document.getElementById('main').innerHTM += "<ul>";
-        for (let i in response){
-            document.getElementById('list').innerHTML += "<li>" + response[i].name + "<ul><li>" + response[i].price + "</li>" + "<li>" +'<img src="' + response[i].imageUrl + '" alt="' + response[i].name + '"></li>' + "<li>" + response[i].description + "</li></ul></li>"; 
+request.onreadystatechange = function() { // fonction qui sera appelée à chaque fois que l'état de la requête change
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200 && document.getElementById('list') != null) { // vérifie que la requête est terminée et que elle s'est bien déroulée
+        var response = JSON.parse(this.responseText); // transofrme le texte JSON de la réponse en objet JS
+        var listeId = []; // crée une liste vide qui va contenir l'Id de chaque article
+        for (let i in response){ // parcourt l'objet pour récupèrer à chaque article son id, name, price, image, puis on modifie notre DOM
+            document.getElementById('list').innerHTML += "<li>" + "<a id='" + response[i]._id + "'" + "href='produit.html'>" + response[i].name + "</a><ul><li>" + response[i].price + "</li>" + "<li>" +'<img src="' + response[i].imageUrl + '" alt="' + response[i].name + '"></li>' + "<li>" + response[i].description + "</li></ul></li>";
+            listeId.push(response[i]._id); // liste des Id des articles
         }
-        //document.getElementById('main').innerHTML += "</ul>";
+    }
+    for (let i in listeId){ // parcourt la liste des Id
+        document.getElementById(listeId[i]).addEventListener('click', function(event) { // écoute les évenements qui contient notre Id, càd nos liens, si il y a click alors on exécute la fonction
+            for (let k in response){ // parcourt notre JSON
+                if (response[k]._id == listeId[i]){ // quand l'Id de l'évènement est retrouvé dans notre JSON, alors :
+                    produitObjet = response[k]; // on y stocke le nom, le price, l'image,... correspondant à Id de l'évenement
+                };
+            };
+      });
+    };
+    if (document.getElementById('name') != null){
+        document.getElementById('name').innerHTML = "Récupérer nom produit" ; // Je voudrai récupéré : produitObjet.name
     }
 };
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
-
-
+request.open("GET", "http://localhost:3000/api/teddies"); // récuperation des données de API
+request.send(); // envoie la requête
